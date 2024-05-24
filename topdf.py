761e59a8a7ddc0d79f1e410c2dd1be4e166c95c1
n1nga <lomakin_2003@list.ru>
2024-05-24 20:40:37 +0300

    Image to pdf base64

--- topdf.py
+++ topdf.py
@@ -5,13 +5,13 @@ import base64
 
 
 def get_image_file_as_base64_data():
-    with open('C:\\Users\\Sergey\\Desktop\\Прога\\scientific-foundations-of-cryology\\VaporcomprDiagram.png', 'rb') as image_file:
+    with open('VaporcomprDiagram.png', 'rb') as image_file:
         return base64.b64encode(image_file.read())
 
 
 def main():
     context = steam_compression_cycle()
-    context['image_string'] = get_image_file_as_base64_data()
+    context['image'] = str(get_image_file_as_base64_data())[2:]
     template_loader = jinja2.FileSystemLoader('./')
     template_env = jinja2.Environment(loader=template_loader)
 
@@ -25,3 +25,4 @@ def main():
 if __name__ == '__main__':
     main()
     print('File has been created')
+
