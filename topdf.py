a62ef92edf488ad0c4e5ef355bdb7d8b7aa506cf
N1ngawolf3 <lomakin_2003@list.ru>
2024-08-04 17:47:41 +0300

    almost everything

--- topdf.py
+++ topdf.py
@@ -3,27 +3,42 @@ import pdfkit
 from main import *
 import base64
 
+funcs = {'ЦПДО': simple_throttling_liq,
+         'ЦПДР': simple_throttling_refr,
+         'ДЦПОР': throttling_prerefr_refr,
+         'ДЦПОО': throttling_prerefr_liq,
+         'ДДР': double_throttling_refr,
+         'ДДО': double_throttling_refr,
+         'ПКЦ': steam_compression_cycle
+         }
 
-def get_image_vaporcomprdiagram():
-    with open('diagrams/steam_compr_graph.png', 'rb') as image_file:
+
+def get_graph_image(cycle_name):
+    with open(f'diagrams/{funcs[cycle_name].__name__}_graph.jpg', 'rb') as image_file:
         return str(base64.b64encode(image_file.read()))[2:]
 
 
 def main():
-    context = simple_throttling_refr()
-    surname = input('Введите фамилию: ')
-    context['image'] = get_image_vaporcomprdiagram()
-    template_loader = jinja2.FileSystemLoader('./')
-    template_env = jinja2.Environment(loader=template_loader)
+    problem = input(f'Выберите задачу из списка: {list(funcs.keys())}\n'
+                    f'Введите задачу (для завершения оставьте поле пустым): ')
+    while problem != '':
+        context = funcs[problem]()
+        surname = input('Введите фамилию: ')
+        number = input('Введите номер варианта: ')
+        context['image'] = get_graph_image(problem)
+        template_loader = jinja2.FileSystemLoader('./')
+        template_env = jinja2.Environment(loader=template_loader)
 
-    template = template_env.get_template('html_temlpates/SimpleThrottlingRefr.html')
-    output_text = template.render(context)
+        template = template_env.get_template(f'html_templates/{funcs[problem].__name__}_template.html')
+        output_text = template.render(context)
 
-    config = pdfkit.configuration(wkhtmltopdf="C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe")
-    pdfkit.from_string(output_text, f'created_pdf/SimpleThrottlingRefr_{surname}.pdf',
-                       configuration=config, css='html_temlpates/vaporcompr.css')
+        config = pdfkit.configuration(wkhtmltopdf="C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe")
+        pdfkit.from_string(output_text, f'created_pdf/{problem}_{surname}_{number}.pdf',
+                           configuration=config, css='html_templates/vaporcompr.css')
+        print('Файл успешно создан')
+        problem = input(f'Выберите задачу из списка: {list(funcs.keys())}\n'
+                        f'Введите задачу (для завершения оставьте поле пустым): ')
 
 
 if __name__ == '__main__':
     main()
-    print('File has been created')
